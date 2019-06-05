010576c4cd786150183e70e821314eef5c17a761
boredStats <ian.abenes@gmail.com>
2019-06-05 11:33:21 -0500

    pep 8 formatting

--- project/proj_utils.py
+++ project/proj_utils.py
@@ -3,10 +3,12 @@
 import os, time, datetime
 import numpy as np
 
+
 def ctime():
     t = time.time()
     f = '%Y-%m-%d %H:%M:%S '
     return datetime.datetime.fromtimestamp(t).strftime(f)
+
 
 def load_connectivity_data(currrent_data_path=None, drop_behavior=True):
     currrent_data_path = './../../data_raw_labeled.pkl'
