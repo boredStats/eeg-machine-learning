594832ea1623b25d702e7484065b122b43c6a1a3
boredStats <ian.abenes@gmail.com>
2019-06-05 12:39:04 -0500

    pep-8 formatting

--- project/eeg_networks.py
+++ project/eeg_networks.py
@@ -11,24 +11,25 @@ logging.basicConfig(level=logging.INFO)
 
 
 def create_adjacency_dict(data_df, bands):
-    colnames = list(data_df)
+    column_names = list(data_df)
 
-    def _extract_band_colnames(band, colnames):
+    def _extract_band_colnames(band_name, colnames):
         names_extracted = []
         for name in colnames:
-            if band in name:
+            if band_name in name:
                 names_extracted.append(name)
 
         return names_extracted
 
-    mat_dict = {}
+    adjacency_dict = {}
     for band in bands:
-        band_variables = _extract_band_colnames(band, colnames)
+        band_variables = _extract_band_colnames(band, column_names)
         adjacency_df = pd.DataFrame(columns=band_variables)
         for bv in band_variables:
             adjacency_df[bv] = data_df[bv]
-        mat_dict[band] = adjacency_df
-    return mat_dict
+        adjacency_dict[band] = adjacency_df
+
+    return adjacency_dict
 
 
 def parse_roi_names(colnames):
@@ -39,8 +40,7 @@ def parse_roi_names(colnames):
     for name in colnames:
         if "delta" in name:
             parsed = name.split("_")
-            roi_1 = parsed[1]
-            roi_2 = parsed[2]
+            roi_1, roi_2 = parsed[1], parsed[2]
             if roi_1 not in roi_list_1:
                 roi_list_1.append(roi_1)
             if roi_2 not in roi_list_2:
@@ -50,32 +50,54 @@ def parse_roi_names(colnames):
 
 
 def create_subj_adjacency_mats(adj_dict, bands, rois, dpath=None):
+    def _create_subject_matrix(roi_list, band_data, diag=1):
+        subj_df = pd.DataFrame(index=roi_list, columns=roi_list)
+        for r1, roi_1 in enumerate(roi_list):
+            for r2, roi_2 in enumerate(roi_list):
+                strcmp_1 = "%s_%s_%s" % (band, roi_1, roi_2)
+                strcmp_2 = "%s_%s_%s" % (band, roi_2, roi_1)
+                for varname in list(band_data):
+                    if strcmp_1 == str(varname) or strcmp_2 == str(varname):
+                        col = band_data[varname].values
+                        val = col[s]
+                        subj_df.iloc[r1, r2] = val
+
+        # Converting diagonals to specified value
+        for r1 in range(len(roi_list)):
+            for r2 in range(len(roi_list)):
+                if r1 == r2:
+                    subj_df.iloc[r1, r2] = diag
+
+        return subj_df
+
     for band in bands:
         band_df = adj_dict[band]
         subjects = band_df.index
         for s, subj in enumerate(subjects):
-            logging.info('Creating matrix for %s_%s' % (band, str(subj)))
-            subject_matrix = pd.DataFrame(index=rois, columns=rois)
             key = '%s_%s' % (band, str(subj))
-            for r1, roi_1 in enumerate(rois):
-                for r2, roi_2 in enumerate(rois):
-                    strcmp_1 = "%s_%s_%s" % (band, roi_1, roi_2)
-                    strcmp_2 = "%s_%s_%s" % (band, roi_2, roi_1)
-                    for varname in list(band_df):
-                        if strcmp_1==str(varname) or strcmp_2==str(varname):
-                            col = band_df[varname].values
-                            val = col[s]
-                            subject_matrix.iloc[r1, r2] = val
+            logging.info('Creating matrix for %s' % key)
 
-            for r1 in range(len(rois)):
-                for r2 in range(len(rois)):
-                    if r1 == r2:
-                        subject_matrix.iloc[r1, r2] = 1
+            # subject_matrix = pd.DataFrame(index=rois, columns=rois)
+            # for r1, roi_1 in enumerate(rois):
+            #     for r2, roi_2 in enumerate(rois):
+            #         strcmp_1 = "%s_%s_%s" % (band, roi_1, roi_2)
+            #         strcmp_2 = "%s_%s_%s" % (band, roi_2, roi_1)
+            #         for varname in list(band_df):
+            #             if strcmp_1==str(varname) or strcmp_2==str(varname):
+            #                 col = band_df[varname].values
+            #                 val = col[s]
+            #                 subject_matrix.iloc[r1, r2] = val
+            #
+            # for r1 in range(len(rois)):
+            #     for r2 in range(len(rois)):
+            #         if r1 == r2:
+            #             subject_matrix.iloc[r1, r2] = 1
 
+            subject_df = _create_subject_matrix(rois, band_df)
             if dpath is not None:
                 fpath = os.path.join(dpath, 'adjacency_%s.pkl' % key)
                 with open(fpath, 'wb') as file:
-                    pkl.dump(subject_matrix, file)
+                    pkl.dump(subject_df, file)
 
 
 def calc_graph_measures(data_matrix, thresh=0):
@@ -139,22 +161,24 @@ def clean_df_to_numpy(df):
     return new_array
 
 
-if __name__ == "__main__":
+def main():
     bands = ['delta', 'theta', 'alpha', 'beta', 'gamma']
-    data_df = proj_utils.load_connectivity_data(drop_behavior=True)
+    # data_df = proj_utils.load_connectivity_data(drop_behavior=True)
+    # rois = parse_roi_names(list(data_df))
 
-    rois = parse_roi_names(list(data_df))
-    print('%s: Creating adjacency dicts' % proj_utils.ctime())
-    adj_dict = create_adjacency_dict(data_df, bands)
-
-    print('%s: Creating subject adjacency matrices' % proj_utils.ctime())
     dpath = os.path.abspath('./../../subject_adjacency_matrices/')
     if not os.path.isdir(dpath):
         os.mkdir(dpath)
-    create_subj_adjacency_mats(adj_dict, bands, rois, dpath)
+
+    # print('%s: Creating adjacency dicts' % proj_utils.ctime())
+    # adj_dict = create_adjacency_dict(data_df, bands)
+    #
+    # print('%s: Creating subject adjacency matrices' % proj_utils.ctime())
+    # create_subj_adjacency_mats(adj_dict, bands, rois, dpath)
 
     test_res = test_graph_functions()
 
+    print('%s: Running graph theory analyses' % proj_utils.ctime())
     final_dict = {}
     columns = list(test_res)
     subjects = np.arange(0, len(os.listdir(dpath)))
@@ -175,3 +199,7 @@ if __name__ == "__main__":
 
     with open(os.path.abspath('./../../graph_theory_res.pkl'), 'wb') as f:
         pkl.dump(final_dict, f)
+
+
+if __name__ == "__main__":
+    main()
