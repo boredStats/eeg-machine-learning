3015ad9918a0346a7e5aafe9381ca5a47868cc4f
boredStats <ian.abenes@gmail.com>
2019-09-03 15:54:04 -0500

    add covariate, class count info to plot

--- project/classification_plotting.py
+++ project/classification_plotting.py
@@ -4,6 +4,7 @@ from itertools import repeat
 from copy import deepcopy
 import numpy as np
 import pandas as pd
+import pickle as pkl
 import seaborn as sns
 import matplotlib as mpl
 import matplotlib.pyplot as plt
@@ -154,7 +155,6 @@ def replot_confusion_matrices():
 
 
 def plot_extra_trees_features():
-    models = ['svm', 'extra_trees', 'sgd', 'knn']
     variables = ['tinnitus_side', 'tinnitus_type', 'TQ_grade', 'TQ_high_low']
 
     conn_data = pu.load_connectivity_data()
@@ -179,26 +179,90 @@ def plot_extra_trees_features():
     for tin_variable in variables:
         output_dir = './../data/%s/' % tin_variable
         for model in listdir(output_dir):
-            tin_dir = join(output_dir, model)
-            xls = pd.ExcelFile(join(tin_dir, 'coefficients.xlsx'))
-            for sheet in xls.sheet_names:
-                band_matrices = deepcopy(band_matrices_master)
-                feature_df = pd.read_excel(xls, sheet_name=sheet, index_col=0)
-                for feat in list(feature_df):
-                    feat_str = feat.split('_')
-                    if any([True for b in bands if b in feat_str]):  # check if feature is connectivity data
-                        feat_band, r1, r2 = feat_str[0], feat_str[1], feat_str[2]
-                        input_matrix = band_matrices[feat_band]
-                        input_matrix.loc[r1][r2] = feature_df[feat].values[0]
+            if 'extra_trees' in model:
+                tin_dir = join(output_dir, model)
+                xls = pd.ExcelFile(join(tin_dir, 'coefficients.xlsx'))
+                fold_data = {}
+                for sheet in xls.sheet_names:
+                    band_matrices = deepcopy(band_matrices_master)
+                    feature_df = pd.read_excel(xls, sheet_name=sheet, index_col=0)
+                    for feat in list(feature_df):
+                        feat_str = feat.split('_')
+                        if any([True for b in bands if b in feat_str]):  # check if feature is connectivity data
+                            feat_band, r1, r2 = feat_str[0], feat_str[1], feat_str[2]
+                            input_matrix = band_matrices[feat_band]
+                            input_matrix.loc[r1][r2] = feature_df[feat].values[0]
+                    fold_data[sheet] = band_matrices
+
+                with open(join(tin_dir, 'connectivity_matrices.pkl'), 'wb') as file:
+                    pkl.dump(obj=fold_data, file=file)
 
 
+def get_variable_data():
+    def _count_data(data_to_count, vartype):
+        data_df = pd.DataFrame(data_to_count, columns=[vartype])
+        count_df = data_df[vartype].value_counts()
+        return count_df
 
+    output_dir = './../data/eeg_classification'
+    if not isdir(output_dir):
+        mkdir(output_dir)
+    behavior_data, conn_data = pu.load_data_full_subjects()
+
+    side_data = pu.convert_tin_to_str(behavior_data['tinnitus_side'].values.astype(float), 'tinnitus_side')
+    side_count = _count_data(side_data, 'Side')
+
+    type_data = pu.convert_tin_to_str(behavior_data['tinnitus_type'].values.astype(float), 'tinnitus_type')
+    type_count = _count_data(type_data, 'Type')
+
+    tq_data = behavior_data['distress_TQ'].values
+    high_low_thresholds = [0, 46, 84]
+    binned_high_low = np.digitize(tq_data, bins=high_low_thresholds, right=True)
+    tq_high_low = ['Low' if t < 2 else 'High' for t in binned_high_low]
+    hl_count = _count_data(tq_high_low, 'TQ (High/Low)')
+
+    grade_thresholds = [0, 30, 46, 59, 84]
+    binned_grade = np.digitize(tq_data, bins=grade_thresholds, right=True)
+    tq_grade = ['Grade_%d' % t for t in binned_grade]
+    grade_count = _count_data(tq_grade, 'TQ (Grade)')
+
+    gender = behavior_data['sex']
+    gender_str = ['Male' if g > 0 else 'Female' for g in gender.values]
+    gender_count = _count_data(gender_str, 'Gender')
+
+    # categorical_variables = ['smoking', 'deanxit_antidepressants', 'rivotril_antianxiety', 'sex']
+    # categorical_data = behavior_data[categorical_variables]
+
+    output = {'side': side_count,
+              'type': type_count,
+              'tq_high_low': hl_count,
+              'tq_grade': grade_count,
+              'gender': gender_count}
+    pu.save_xls(output, join(output_dir, 'tin_variables_classcount.xlsx'))
+
+
+def plot_age_historgram(output_dir=None):
+    behavior_data, conn_data = pu.load_data_full_subjects()
+    age = behavior_data['age']
+    sns.set_style('darkgrid')
+    fig, ax = plt.subplots(figsize=(8, 6))
+    sns.distplot(age.values, kde=False, ax=ax, hist_kws={"alpha": .75, "color": 'b'})
+    ax.set_xlabel('Age')
+    ax.set_ylabel('Frequency')
+    if output_dir is None:
+        plt.show()
+    else:
+        fig.savefig(join(output_dir, 'age_hist.png'))
 
 
 if __name__ == "__main__":
-    models = ['svm', 'extra_trees', 'sgd', 'knn']
+    output_dir = './../data/eeg_classification'
+    models = ['extra_trees']  # ['svm', 'extra_trees', 'sgd', 'knn']
     variables = ['tinnitus_side', 'tinnitus_type', 'TQ_grade', 'TQ_high_low']
     # boxplot_testing()
     # conf_mat_testing(fname='test_confusion_matrix.png')
     # replot_confusion_matrices()
-    plot_extra_trees_features()
+    # plot_extra_trees_features()
+    # get_variable_data()
+
+    plot_age_historgram(output_dir)
